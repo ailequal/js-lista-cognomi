af69df25cc27f8459ceed299060ed19661d56aa1
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-08 15:26:01 +0100

    Adding some js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+// existing arraySurname
+var arraySurname = ['Rossi', 'Verdi', 'Serafini', 'Pronti', 'Protti', 'Tonti', 'Tonini', 'Magnani', 'Verducci', 'Esati'];
+console.log(arraySurname);
+
+// ask userSurname
+var userSurname = prompt("Insert your surname");
+console.log(userSurname);
+
+// insert inside an existing array
+arraySurname.push(userSurname);
+console.log(arraySurname);
+
+// print array sorted alphabetically html ul li
+
+
+// print the surname position of the user
